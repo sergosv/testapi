e473bc037f31faa49438d19d28191fc765ede8a8
Sergio Sanchez <sergio_sv@prodigy.net.mx>
2019-05-02 18:26:50 +0000

    todos los archivos

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ var Connection = require('tedious').Connection;
          executeStatement();
     }
   );
+
+var Request = require('tedious').Request;
+  function executeStatement() {
+    request = new Request("select 42, 'hello world'", function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rowCount + ' rows');
+      }
+    });
+    request.on('row', function(columns) {
+      columns.forEach(function(column) {
+        console.log(column.value);
+      });
+    });
+    connection.execSql(request);
+  }
